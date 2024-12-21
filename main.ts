3efa03e7f14953c8df8d99e11ae4d9b162a28d78
dxxztoys <834830866@qq.com>
2024-12-21 10:14:05 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 /**
  * Control micro servos
  */
-//% color="#03AA74" weight=88 icon="\uf021" blockGap=8
+//% color="#03AA74" weight=24 icon="\uf021" blockGap=8
 //% blockId="servos" block="舵机模块"
 namespace servos {
-	//% weight=100
+	//% weight=50
 	//% blockId=servoservosetangle block="设置舵机 %servo 角度 %degrees °"
 	//% degrees.defl=90
 	//% blockGap=8
@@ -12,8 +12,8 @@ namespace servos {
         // send pulse
         pins.servoSetPulse(servo, Math.map(degrees, 0, 180, 500, 2500))
     }
-	//% weight=100
-	//% blockId=servoservosetpulse block="设置舵机 %servo2 脉冲为 %pulse 微妙"
+	//% weight=20
+	//% blockId=servoservosetpulse block="设置舵机 %servo2 脉冲为 %pulse 微秒(μs)"
 	//% pulse.defl=1500
 	//% blockGap=8
     export function Servo_pulse(servo2: AnalogPin, pulse: number=1500): void {
